0afc15aaf2b51996a97276036080866fb109a74b
Steve Brandt <steve.brandt@springer.com>
2021-11-12 14:52:40 +0100

    clean up

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
 const { typeDefs }= require('./schema/TypeDefs')
 const { resolvers }= require('./schema/Resolvers')
 
--- schema/Resolvers.js
+++ schema/Resolvers.js
@@ -3,9 +3,7 @@
 // A map of functions which return data for the schema.
 const resolvers = {
     Query: {
-        teasers() {
-            return [{title:'title', body:'body', link: 'link'}]
-        },
+        teasers: () => [{title:'title', body:'body', link: 'link'}],
         hello: () => 'Hello world!'
     }
 };
